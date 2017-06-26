f55d80e20f22588c3f42ddd723deb468fd354cee
cafeunder <cafe3641@gmail.com>
2017-06-27 06:20:50 +0900

    ベンチマークを行うメソッドを実装

--- TSPSolver/Benchmarker.cs
+++ TSPSolver/Benchmarker.cs
@@ -4,17 +4,25 @@ using TSPSolver.solver;
 
 namespace TSPSolver {
 	public class Benchmarker {
-		public static void Main(string[] args) {
+		private static string[] INSTANCES = {
+			"ca4663",
+			"fi10639",
+			"bm33708",
+			"ch71009",
+			"mona-lisa100K"
+		};
+
+		public static void Run(string instanceName) {
 			// 使うインスタンス
-			TSPInstance instance = new TSPInstance(@"data/ca4663.tsp");
+			TSPInstance instance = new TSPInstance(@"data/" + instanceName + ".tsp");
 			// 試行回数
 			int trial_num = 10;
 			// 経路長の合計、経路長の2乗の合計
-			int length_sum = 0;
-			int length_sum2 = 0;
+			double length_sum = 0;
+			double length_sum2 = 0;
 			// 計算時間の2乗の合計
-			long time_sum = 0;
-			long time_sum2 = 0;
+			double time_sum = 0;
+			double time_sum2 = 0;
 
 			Stopwatch sw = new Stopwatch();
 			for (int i = 0; i < trial_num; i++) {
@@ -26,22 +34,30 @@ namespace TSPSolver {
 				// 経路長の計上
 				int length = instance.CalcTourLength(tour);
 				length_sum += length;
-				length_sum2 += length * length;
+				length_sum2 += (double)length * length;
 
 				// 計算時間の計上
 				time_sum += sw.ElapsedMilliseconds;
-				time_sum2 += sw.ElapsedMilliseconds * sw.ElapsedMilliseconds;
+				time_sum2 += (double)sw.ElapsedMilliseconds * sw.ElapsedMilliseconds;
 				sw.Reset();
 			}
 
 			double length_ave = (length_sum / (double)trial_num);
 			double length_sd = Math.Sqrt(length_sum2 / (trial_num - 1.0) - (trial_num) / (trial_num - 1.0) * length_ave * length_ave);
+			Console.WriteLine(length_sum2 / (trial_num - 1.0));
+			Console.WriteLine((trial_num) / (trial_num - 1.0) * length_ave * length_ave);
 			double time_ave = (time_sum / (double)trial_num);
 			double time_sd = Math.Sqrt(time_sum2 / (trial_num - 1.0) - (trial_num) / (trial_num - 1.0) * time_ave * time_ave);
 
 			Console.WriteLine("result : ave[sd]");
 			Console.WriteLine("length : " + length_ave + "[" + length_sd + "]");
 			Console.WriteLine("time   : " + time_ave + "[" + time_sd + "]");
+		}
+
+		public static void Main(string[] args) {
+			foreach (string instanceName in INSTANCES) {
+				Benchmarker.Run(instanceName);
+			}
 		}
 	}
 }
