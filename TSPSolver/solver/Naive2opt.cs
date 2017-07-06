4e35d98739fe3af0c85df7fc43b24e60a370c8f8
cafeunder <cafe3641@gmail.com>
2017-07-06 23:32:46 +0900

    ナイーブ実装2optで正順のみをチェックするように変更

--- TSPSolver/solver/Naive2opt.cs
+++ TSPSolver/solver/Naive2opt.cs
@@ -17,35 +17,30 @@ namespace TSPSolver.solver {
 			int si = SRandom.Instance.NextInt(instance.Dimension);
 			for (int i = 0; i < instance.Dimension; i++) {
 				int v = (si + i) % instance.Dimension;
-				
-				// 正順と逆順のエッジを確かめる
-				for (int d = 0; d < 2; d++) {
-					bool forward = (d == 0);
-					int vn = (forward) ? tour.NextID(v) : tour.PrevID(v);
+				int vn = tour.NextID(v);
 
-					// ランダムなノードsから始めて全てのノードに接続しているエッジを検索する
-					int sj = SRandom.Instance.NextInt(instance.Dimension);
-					for (int j = 0; j < instance.Dimension; j++) {
-						int w = (sj + j) % instance.Dimension;
-						if (w == v) { continue; }
+				// ランダムなノードsから始めて全てのノードに接続しているエッジを検索する
+				int sj = SRandom.Instance.NextInt(instance.Dimension);
+				for (int j = 0; j < instance.Dimension; j++) {
+					int w = (sj + j) % instance.Dimension;
+					if (w == v) { continue; }
+					int wn = tour.NextID(w);
 
-						int wn = (forward) ? tour.NextID(w) : tour.PrevID(w);
-						// (v, vn)と(w, wn)を削除する
-						int remove_gain = instance.CalcDistance(v, vn) + instance.CalcDistance(w, wn);
-						// (v, w)と(vn, wn)を追加する
-						int add_gain = instance.CalcDistance(v, w) + instance.CalcDistance(vn, wn);
+					// (v, vn)と(w, wn)を削除する
+					int remove_gain = instance.CalcDistance(v, vn) + instance.CalcDistance(w, wn);
+					// (v, w)と(vn, wn)を追加する
+					int add_gain = instance.CalcDistance(v, w) + instance.CalcDistance(vn, wn);
 
-						if (add_gain < remove_gain) {
-							tour.Flip(v, w, forward);
+					if (add_gain < remove_gain) {
+						tour.Flip(v, w, true);
 #if DEBUG
-							length += add_gain - remove_gain;
-							Console.WriteLine(length + ", " + instance.CalcTourLength(tour.NodeArray));
+						length += add_gain - remove_gain;
+						Console.WriteLine(length + ", " + instance.CalcTourLength(tour.NodeArray));
 #endif
-							// 最初からやりなおす
-							i = 0;
-							si = SRandom.Instance.NextInt(instance.Dimension);
-							goto FINISH;
-						}
+						// 最初からやりなおす
+						i = 0;
+						si = SRandom.Instance.NextInt(instance.Dimension);
+						goto FINISH;
 					}
 				}
 			FINISH:;
